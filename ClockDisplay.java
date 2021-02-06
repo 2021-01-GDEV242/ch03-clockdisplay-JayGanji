b9150b117b30f87d8e61cd6a6c8ff73552c95f40
JayGanji <jayaditya.ganji@gmail.com>
2021-02-06 17:59:08 -0500

    updating version and authors

--- ClockDisplay.java
+++ ClockDisplay.java
@@ -9,8 +9,8 @@
  * and reacts by incrementing the display. This is done in the usual clock
  * fashion: the hour increments when the minutes roll over to zero.
  * 
- * @author Michael Kölling and David J. Barnes
- * @version 2016.02.29
+ * @author Michael Kölling and David J. Barnes, modified by Jay Ganji
+ * @version 2021.02.06
  */
 public class ClockDisplay
 {
